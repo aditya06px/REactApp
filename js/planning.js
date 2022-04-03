2356f4ab30269b6e91d43cc3788b972f7bb068ec
Aditya Lavate <84095872+aditya06px@users.noreply.github.com>
2022-04-03 09:53:43 +0530

    Add files via upload

---
+++ js/planning.js
@@ -0,0 +1,20 @@
+//Book my show
+
+// we need a carousel
+// --Hero carousel 
+// --common carousel
+// -- cast n crew small carousel
+
+//pages
+
+// --home page
+// --movie page 
+// --categories page
+// --authentication page
+// --razorpay payment gateway
+
+//how 
+// --layout-navbar,footer
+// --routes
+// --pages
+// --compoents
